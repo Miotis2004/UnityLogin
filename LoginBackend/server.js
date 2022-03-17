d62bd4909ea6262ac70932c0ee8c51a28ffb4afb
Miotis2004 <miotis2004@gmail.com>
2022-03-17 15:57:16 -0700

    Added key storage

--- LoginBackend/server.js
+++ LoginBackend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const keys = require('./config/keys.js');
 
 const app  = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Routes
 app.get("/auth", async (req, res) => {
     res.send("Hello World! " + Date.now());
 });
 
-const port = 13756;
-app.listen(port, () => {
-    console.log("Listening on " + port.toString());
+app.listen(keys.port, () => {
+    console.log("Listening on " + keys.port.toString());
 })
