ac87319a26d2e156117b97b4ef305e1d3dfd1a2a
Miotis2004 <miotis2004@gmail.com>
2022-03-17 17:04:45 -0700

    Connecting to DB

--- LoginBackend/server.js
+++ LoginBackend/server.js
@@ -5,10 +5,41 @@ const app  = express();
 
 const mongoose = require('mongoose');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+require('./model/Account');
+const Account = mongoose.model('accounts');
 
 //Routes
-app.get("/auth", async (req, res) => {
-    res.send("Hello World! " + Date.now());
+app.get("/account", async (req, res) => {
+    const{rUsername, rPassword} = req.query;
+
+    if (rUsername == null || rPassword == null) {
+        res.send('Invalid credentials');
+        return;
+    }
+
+    var userAccount = await Account.findOne({username: rUsername});
+    if(userAccount == null) {
+        console.log('Create new account...');
+        
+        var newAccount = new Account({
+            username: rUsername,
+            password: rPassword,
+            lastAuthentication: Date.now()
+        });
+        await newAccount.save();
+
+        res.send(newAccount);
+        return;
+    } else {
+        if(rPassword == userAccount.password) {
+            userAccount.lastAuthentication = Date.now();
+            await userAccount.save();
+
+            res.send(userAccount);
+        }
+    }
+
+    res.send('hello');
 });
 
 app.listen(keys.port, () => {
